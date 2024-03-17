7b22e9fffd047d44c06bc07d1e7b8a53914d9529
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2024-03-17 22:37:33 +0530

    Initialize chore-clean-up

---
+++ ui/utils/custom-column.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Card,
+  Checkbox,
+  ClickAwayListener,
+  IconButton,
+  Tooltip,
+} from '@layer5/sistent-components';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Popper from '@material-ui/core/Popper';
+import { withStyles } from '@material-ui/core/styles';
+import ColumnIcon from '../assets/icons/coulmn';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+
+const styles = (theme) => ({
+  epaper: {
+    '&: .MuiPaper-root': {
+      background: theme.palette.secondary.headerColor,
+      color: theme.palette.secondary.textMain,
+    },
+    padding: '1rem',
+    boxShadow:
+      '0px 4px 0px -2px rgb(0 179 159 / 10%), 0px 4px 0px -2px rgb(0 179 159 / 10%), 0px 4px 0px -2px rgb(0 179 159 / 10%)',
+  },
+  icon: {
+    color: theme.palette.secondary.iconMain,
+  },
+});
+
+const CustomColumnVisibilityControl = ({ columns, customToolsProps, classes }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleColumnVisibilityChange = (columnName, isVisible) => {
+    customToolsProps.setColumnVisibility((prevState) => ({
+      ...prevState,
+      [columnName]: isVisible,
+    }));
+  };
+
+  return (
+    <div>
+      <Tooltip title="View Columns">
+        <IconButton
+          onClick={handleOpen}
+          sx={{
+            '&:hover': {
+              borderRadius: '4px',
+            },
+          }}
+        >
+          <ColumnIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Box sx={{ overflow: 'hidden' }}>
+        <Popper
+          style={{ zIndex: 120 }}
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          placement="bottom-start"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+
+          // These code is commented out now because it was having anomalies in the UI at different sections while slide in,
+          // had to find better solution for this which will taken care from sistent by spring animation because the clickawaylistner
+          // having issues while having inline transition style/slider in the popper.
+
+          // transition
+          // popperOptions={{
+          //   modifiers: [
+          //     {
+          //       name: 'offset',
+          //       options: {
+          //         offset: [0, 8],
+          //       },
+          //     },
+          //     {
+          //       name: 'preventOverflow',
+          //       options: {
+          //         altAxis: true, // true by default
+          //         tether: true,
+          //         rootBoundary: 'document',
+          //         padding: 8,
+          //       },
+          //     },
+          //   ],
+          // }}
+        >
+          {/* {({ TransitionProps }) => (
+            <Slide
+              {...TransitionProps}
+              direction="down"
+              in={open}
+              timeout={transitionDuration}
+              mountOnEnter
+              unmountOnExit
+              container={containerRef.current}
+              easing="cubic-bezier(0.25, 0.1, 1, 1)"
+            > */}
+          <Box>
+            <ClickAwayListener onClickAway={handleClose}>
+              <Card className={classes.epaper} elevation={2}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  {columns.map((col) => (
+                    <FormControlLabel
+                      key={col.name}
+                      control={
+                        <Checkbox
+                          className={classes.checkbox}
+                          checked={customToolsProps.columnVisibility[col.name]}
+                          onChange={(e) => handleColumnVisibilityChange(col.name, e.target.checked)}
+                          icon={<CheckBoxOutlineBlankIcon className={classes.icon} />}
+                          sx={{
+                            '&.Mui-checked': {
+                              color: '#00B39F',
+                            },
+                          }}
+                        />
+                      }
+                      label={col.label}
+                    />
+                  ))}
+                </div>
+              </Card>
+            </ClickAwayListener>
+          </Box>
+          {/* </Slide>
+          )} */}
+        </Popper>
+      </Box>
+    </div>
+  );
+};
+
+export default withStyles(styles)(CustomColumnVisibilityControl);
